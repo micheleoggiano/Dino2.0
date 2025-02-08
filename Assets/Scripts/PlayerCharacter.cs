a9674523c5d9b74cb904afbf5d972604a914175a
micheleoggiano <oggianomichele23@gmail.com>
2025-02-08 12:27:20 +0100

    Script Updated: Jump and GetAxis

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -8,14 +8,27 @@ using UnityEngine.Rendering;
 
 public class PlayerCharacter : MonoBehaviour
 {
-public float maxSpeed = 1f;
-    
+    public float maxSpeed = 6f;
+    public Vector2 direction;
+    public float jumpHeight = 3f;
+
+    // Creo booleano per isGrounded
+    bool isGrounded = true;
+
+    // Crea referenza Rigidbody2D:
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+    OnCollisionExit
     // Update is called once per frame
     void Update()
     {
@@ -24,20 +37,45 @@ public float maxSpeed = 1f;
             maxSpeed = maxSpeed * 2f;
 
        if (Input.GetKeyUp(KeyCode.LeftShift))
-            maxSpeed = 2f;
+            maxSpeed = 6f;
 
-    // Player movement WASD:
-        if (Input.GetKey(KeyCode.W))
-            transform.position += Vector3.up * maxSpeed * Time.deltaTime;
+        // Player Movement WASD con GetAxis:
 
-        if (Input.GetKey(KeyCode.S))
-            transform.position += -Vector3.up * maxSpeed * Time.deltaTime;
+        float x;
+        float y;
 
-        if (Input.GetKey(KeyCode.D))
-            transform.position += Vector3.right * maxSpeed * Time.deltaTime;
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.A))
-            transform.position += -Vector3.right * maxSpeed * Time.deltaTime;
+        direction = new Vector2(x, y);
+
+        transform.position += maxSpeed * Time.deltaTime * (Vector3)direction;
+
+        // Capacità di salto:
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpVelocity = Mathf.Sqrt(2f * Phisics2D.gravity.magnitude * rb.gravityScale * jumpHeight);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.velocity = Vector2.up * jumpHeight;
+            }
+
+            isGrounded = false;
+        }
+
+
+    // Player movement WASD con "GetKey":
+        //if (Input.GetKey(KeyCode.W))
+        //    transform.position += Vector3.up * maxSpeed * Time.deltaTime;
+
+        //if (Input.GetKey(KeyCode.S))
+        //    transform.position += -Vector3.up * maxSpeed * Time.deltaTime;
+
+        //if (Input.GetKey(KeyCode.D))
+        //    transform.position += Vector3.right * maxSpeed * Time.deltaTime;
+
+        //if (Input.GetKey(KeyCode.A))
+        //    transform.position += -Vector3.right * maxSpeed * Time.deltaTime;
 
         
             
